28f98d8a154c3eb2f486addd2f9919fb88775d9d
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2018-02-22 10:31:41 +1100

    ERICKSON, Bryce: 2018-02-22 10:31:41

---
+++ user/m8ball.py
@@ -0,0 +1,46 @@
+# trying to get the code to make a inital randomizer for the 8 ball
+from random import randint
+Question=input('ask a question')
+answers=['yes', 'no', 'maybe','of corse not','indefently','my sources say no','my sources say yes','you need to ask again later','impossible','your nosey butt said no' ]            
+brycenumber=randint(0,9)
+print(answers[randint (0,9)])
+Question=input('do you want to ask another question')
+answers=['yes', 'no', 'maybe','of corse not','indefently','my sources say no','my sources say yes','you need to ask again later','impossible','your nosey butt said no' ]            
+brycenumber=randint(0,9)
+print(answers[randint (0,9)])
+Question=input('do you want to ask another question')
+answers=['yes', 'no', 'maybe','of corse not','indefently','my sources say no','my sources say yes','you need to ask again later','impossible','your nosey butt said no' ]            
+brycenumber=randint(0,9)
+print(answers[randint (0,9)])
+question=input('do you want to ask another question')
+answers=['yes', 'no', 'maybe','of corse not','indefently','my sources say no','my sources say yes','you need to ask again later','impossible','your nosey butt said no' ]            
+brycenumber=randint(0,9)
+print(answers[randint (0,9)])
+question=input('do you want to ask another question')
+answers=['yes', 'no', 'maybe','of corse not','indefently','my sources say no','my sources say yes','you need to ask again later','impossible','your nosey butt said no' ]            
+brycenumber=randint(0,9)
+print(answers[randint (0,9)]) 
+question=input('do you want to ask another question')
+answers=['yes', 'no', 'maybe','of corse not','indefently','my sources say no','my sources say yes','you need to ask again later','impossible','your nosey butt said no' ]            
+brycenumber=randint(0,9)
+print(answers[randint (0,9)]) 
+question=input('do you want to ask another question')
+answers=['yes', 'no', 'maybe','of corse not','indefently','my sources say no','my sources say yes','you need to ask again later','impossible','your nosey butt said no' ]            
+brycenumber=randint(0,9)
+print(answers[randint (0,9)]) 
+question=input('do you want to ask another question')
+answers=['yes', 'no', 'maybe','of corse not','indefently','my sources say no','my sources say yes','you need to ask again later','impossible','your nosey butt said no' ]            
+brycenumber=randint(0,9)
+print(answers[randint (0,9)]) 
+question=input('do you want to ask another question')
+answers=['yes', 'no', 'maybe','of corse not','indefently','my sources say no','my sources say yes','you need to ask again later','impossible','your nosey butt said no' ]            
+brycenumber=randint(0,9)
+print(answers[randint (0,9)]) 
+question=input('do you want to ask another question')
+answers=['yes', 'no', 'maybe','of corse not','indefently','my sources say no','my sources say yes','you need to ask again later','impossible','your nosey butt said no' ]            
+brycenumber=randint(0,9)
+print(answers[randint (0,9)]) 
+question=input('do you want to ask another question')
+answers=['yes', 'no', 'maybe','of corse not','indefently','my sources say no','my sources say yes','you need to ask again later','impossible','your nosey butt said no' ]            
+brycenumber=randint(0,9)
+print(answers[randint (0,9)]) 
