19ab898b72e8b91f8f8a05cde1f2fc44950a7ea1
Erik Koopmans <koopmans.erik.m@edumail.vic.gov.au>
2018-03-02 08:59:55 +1100

    ERICKSON, Bryce: 2018-03-02 08:59:55

---
+++ user/igpay atilay copy.py
@@ -0,0 +1,6 @@
+# will create a basic pig latin translator
+date=input('what day is today')
+if date=="Sunday":
+    print('sleep in')
+else:
+    print('go to school')
--- user/m8ball.py
+++ user/m8ball.py
@@ -1,7 +1,7 @@
 # trying to get the code to make a inital randomizer for the 8 ball
 from random import randint
 Question=input('ask a question')
-answers=['yes', 'no', 'maybe','of corse not','indefently','my sources say no','my sources say yes','you need to ask again later','impossible','your nosey butt said no' ]            
+answers=['yes', 'no', 'maybe','of coruse not','indefently','my sources say no','my sources say yes','you need to ask again later','impossible','your nosey butt said no' ]            
 brycenumber=randint(0,9)
 print(answers[randint (0,9)])
 Question=input('do you want to ask another question')
@@ -16,10 +16,6 @@ question=input('do you want to ask another question')
 answers=['yes', 'no', 'maybe','of corse not','indefently','my sources say no','my sources say yes','you need to ask again later','impossible','your nosey butt said no' ]            
 brycenumber=randint(0,9)
 print(answers[randint (0,9)])
-question=input('do you want to ask another question')
-answers=['yes', 'no', 'maybe','of corse not','indefently','my sources say no','my sources say yes','you need to ask again later','impossible','your nosey butt said no' ]            
-brycenumber=randint(0,9)
-print(answers[randint (0,9)]) 
 question=input('do you want to ask another question')
 answers=['yes', 'no', 'maybe','of corse not','indefently','my sources say no','my sources say yes','you need to ask again later','impossible','your nosey butt said no' ]            
 brycenumber=randint(0,9)
